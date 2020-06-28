b97f1718ede55591c03a2d4c0f44c37caacb200a
the-promised-LAN <k.vas97@yahoo.com>
2020-06-27 20:20:02 -0400

    first commit

---
+++ Assets/barracuda_inference.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using Unity.Barracuda;
+
+public class barracuda_inference : MonoBehaviour
+{
+    // Barracuda variables
+    public NNModel modelSource;     // ONNX model (asset)
+    public Model model;             // Runtime model wrapper (binary)
+    private IWorker worker;         // Barracuda worker for inference
+    public Sprite sprite1, sprite2; // Images to be classified (assets)
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        model = ModelLoader.Load(modelSource);      // Load ONNX model as runtime binary model 
+        worker = WorkerFactory.CreateWorker(WorkerFactory.Type.ComputePrecompiled, model);  // Create Worker 
+
+        Classify();     // Calling it once
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /**
+     * Runs inference on two images
+     */
+    private void Classify()
+    {
+        // convert WebCamTexture to Tensor
+        var channels = 3;   // color; use 1 for grayscale, 4 for alpha
+
+        // Get pixels from dog sprite
+        var dog_px = sprite1.texture.GetPixels(
+            (int)sprite1.textureRect.x,
+            (int)sprite1.textureRect.y,
+            (int)sprite1.textureRect.width,
+            (int)sprite1.textureRect.height);
+
+        // Get pixels from cat sprite
+        var cat_px = sprite2.texture.GetPixels(
+            (int)sprite2.textureRect.x,
+            (int)sprite2.textureRect.y,
+            (int)sprite2.textureRect.width,
+            (int)sprite2.textureRect.height);
+
+        Texture2D input = new Texture2D(150, 150);      // Creating Texture2D and setting pixels to dog image
+        input.SetPixels(dog_px);
+        input.Apply();
+
+        // ==== Inference on a dog picture (should return 1) ====
+
+        var in_tensor = new Tensor(input, channels);    // Create tensor from Texture2D (3 channel)
+        UnityEngine.Debug.Log("Got preprocessed tensor...");
+
+        worker.Execute(in_tensor);                      // run inference on tensor
+        UnityEngine.Debug.Log("Model executed on dog photo...");
+
+        // Explore inference detections
+        var out_tensor = worker.PeekOutput("dense_5");  // name of output layers; 'predictions' for Xception NN
+        var max_val = Mathf.Max(out_tensor.ToReadOnlyArray());
+        var arr = out_tensor.ToReadOnlyArray();
+        var index = System.Array.IndexOf(arr, max_val);
+
+        UnityEngine.Debug.Log("Output = " + out_tensor[0]);     // should be approx. 1 for dogs
+        UnityEngine.Debug.Log("Max prob = " + max_val);
+        UnityEngine.Debug.Log("Index of max = " + index);
+
+        // ==== Inference on a cat picture (should return 0) ====
+
+        input.SetPixels(cat_px);                        // Set Texture2D pixels to cat photo
+        input.Apply();
+        in_tensor = new Tensor(input, channels);        // Convert to tensor
+
+        worker.Execute(in_tensor);                      // run inference on tensor
+        UnityEngine.Debug.Log("Model executed on cat photo...");
+
+        // Explore inference detections
+        out_tensor = worker.PeekOutput("dense_5");      // name of output layers; 'predictions' for Xception NN
+        max_val = Mathf.Max(out_tensor.ToReadOnlyArray());
+        arr = out_tensor.ToReadOnlyArray();
+        index = System.Array.IndexOf(arr, max_val);
+
+        UnityEngine.Debug.Log("Output = " + out_tensor[0]);   // Should be approx. 0 for cats
+        UnityEngine.Debug.Log("Max prob = " + max_val);
+        UnityEngine.Debug.Log("Index of max = " + index);
+
+        // Clean up (don't forget!)
+        worker.Dispose();
+        in_tensor.Dispose();
+        out_tensor.Dispose();     // not necessary if gathered by worker.PeekOutput()
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # unity_barracuda_keras_example
 Quick and dirty example of using Unity Barracuda v1.0 for inference using TensorFlow/Keras-created models
+using a custom made and trained CNN in Keras (TensorFlow v2.1 backend) on [Dogs vs. Cats dataset](https://www.kaggle.com/c/dogs-vs-cats). 
+
+[barracuda_inference.cs]() explains how to perform inference step-by-step
+
+[If you're looking on how to convert Keras' *.h5* models to *.ONNX*, check out example at this repo](https://github.com/the-promised-LAN/keras_to_onnx_example).
+
+### Requirements
+* Unity 2019.x (didn't test on older versions, but confirmed working on all 2019 versions)
+* Unity Barracuda v1.0 (installation instructions: https://docs.unity3d.com/Packages/com.unity.barracuda@1.0/manual/index.html)
+
+### Description
+There are two provided images, *cat2.png* and *dog2.png*, both 150x150 RGB images (required by model) on which inference is run. The script is the most barebones needed for inference on images; can be a good starting point for further development.
+
+[Don't forget to check out official docs for Barracuda here!!!](https://docs.unity3d.com/Packages/com.unity.barracuda@1.0/manual/index.html)
+
+### How to use
+* Create a new 2D project in Unity/Unity Hub
+* Install Unity Barracuda v1.0 package (see link under Requirements)
+* Import all files in Assets folder into the project's assets (drag and drop)
+* Select *cat2* and *dog2* images in Project window inside Unity, and inside Inspector window, check *Read/Write Enabled* under Advanced
+* Select Main Camera in the Hierarchy window, go to the Inspector window, click Add Component, and select *barracuda_inference* scritpt. 
+* In the same Inspector window (with Main Camera still selected under Hierarchy), drag and drop *cats_dog_small_2.onnx*, *dog2.png* into Sprite1, and *cat2.png* from Assets into Sprite2 attribute
+* Press Run from within Unity, and observe output in the Console. Output is approx. 0 for cat photos, and
+approx. 1 for Dog photos.
+
